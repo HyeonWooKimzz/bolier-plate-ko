3594a8ca1649625fffcc7f9325683605783dfee8
unknown <rlagusdn1356@naver.com>
2025-09-22 20:18:55 +0900

    Security

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hwkim:IXsWKkc8okubA1mZ@cluster2.k1zt5ei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2', {
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extend: true}));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected..'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello'))
+app.get('/', (req, res) => res.send('Hello2'))
+
+app.post('/register', async (req, res) => {
+    // 회원가입 시 필요한 정보들을 Client에서 가져오면
+    // DB에 넣어줌
+    try {
+        const user = new User(req.body)
+        await user.save()
+        return res.status(200).json({ success: true})
+    } catch (err) {
+        return res.status(400).json({ success: false, err})
+    }
+})
+
+
 
 app.listen(port, () => console.log(`Example ${port}`))
