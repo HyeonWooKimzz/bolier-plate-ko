2e4b6f458c574670b614830c45e0f1dd2a2476f0
unknown <rlagusdn1356@naver.com>
2025-09-25 18:54:54 +0900

    gpt 질문용

--- client/src/index.js
+++ client/src/index.js
@@ -5,20 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import { thunk as ReduxThunk } from 'redux-thunk';
 import Reducer from './_reducers';
 
-const creatStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+// Redux DevTools Extension 연결 (있는 경우에만)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// 스토어 생성: 미들웨어 + DevTools enhancer 함께 사용
+const store = createStore(
+  Reducer,
+  composeEnhancers(applyMiddleware(promiseMiddleware, ReduxThunk))
+);
+
+// React 앱 렌더링
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={creatStoreWithMiddleware(Reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
